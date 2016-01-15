b21a90c69ee1d81d1adcc59311e9f776cc92cd0d
drandreaskrueger <study@andreaskrueger.de>
2016-01-15 18:53:17 +0100

    confidential private config information - must fill in, but: careful!

---
+++ buyme/configPrivate.py
@@ -0,0 +1,14 @@
+
+# Coinbase API credentials:
+API_KEY=""
+API_SECRET=""
+
+# where to send the alert "customer paid!" to
+EMAIL_ALERT_ME=""  
+
+# I use gmail to send alerts: 
+EMAIL_HOST_USER=""            # gmail username
+EMAIL_HOST_PASSWORD=""        # gmail password
+EMAIL_SENDER=EMAIL_HOST_USER
+
+
