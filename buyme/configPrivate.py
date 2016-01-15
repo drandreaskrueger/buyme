e16c028852ee5d7e701826fbc04a1182d2d22154
drandreaskrueger <study@andreaskrueger.de>
2016-01-15 19:08:40 +0100

    confidential private config information - must fill in

--- buyme/configPrivate.py
+++ buyme/configPrivate.py
@@ -1,10 +1,15 @@
+# Private credentials. Careful with this information. 
+# Yes, the filename 'configPrivate.py' is in .gitignore
+# but perhaps there are more secure ways. Tell me, thx.
 
 # Coinbase API credentials:
 API_KEY=""
 API_SECRET=""
 
-# where to send the alert "customer paid!" to
-EMAIL_ALERT_ME=""  
+
+# optional:
+# where to send the alert "customer paid!" to?
+EMAIL_ALERT_ME="" # if this is =="" then no email is sent.  
 
 # I use gmail to send alerts: 
 EMAIL_HOST_USER=""            # gmail username
