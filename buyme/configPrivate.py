93ac838815d97da211235a556b4ac474d1ad8e87
drandreaskrueger <drandreaskrueger@users.noreply.github.com>
2016-01-19 11:33:25 +0100

    keep coinbase urls with api_keys, easier to switch sandbox<->production

--- buyme/configPrivate.py
+++ buyme/configPrivate.py
@@ -2,16 +2,32 @@
 # Yes, the filename 'configPrivate.py' is in .gitignore
 # but perhaps there are more secure ways. Tell me, thx.
 
+PRODUCTION=False  
+
 # Coinbase API credentials:
-API_KEY=""
-API_SECRET=""
+
+if not PRODUCTION:
+  # Sandbox 
+  API_KEY=""
+  API_SECRET=""
+
+else:
+  # PRODUCTION
+  API_KEY = ""
+  API_SECRET =""
+
+SANDBOX_URLS = ('https://api.sandbox.coinbase.com', 'https://sandbox.coinbase.com')
+PRODUCTION_URLS=(      'https://api.coinbase.com' , 'https://coinbase.com')
+API_BACKEND_URL, API_FRONTEND_URL=PRODUCTION_URLS if PRODUCTION else SANDBOX_URLS 
 
 
 # optional:
+
 # where to send the alert "customer paid!" to?
-EMAIL_ALERT_ME="" # if this is =="" then no email is sent.  
+EMAIL_ALERT_ME=""   # if this is =="" then no email is sent.  
 
 # I use gmail to send alerts: 
-EMAIL_HOST_USER=""            # gmail username
-EMAIL_HOST_PASSWORD=""        # gmail password
+EMAIL_HOST_USER=""      # gmail username
+EMAIL_HOST_PASSWORD=""  # gmail password
 EMAIL_SENDER=EMAIL_HOST_USER
+
